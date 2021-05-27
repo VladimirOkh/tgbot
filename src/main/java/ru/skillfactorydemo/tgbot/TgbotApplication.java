4c6769581145a95d68e545e57a8e3067edcd5b14
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2021-05-27 18:33:53 +0300

    Changed folder

--- src/main/java/ru/skillfactorydemo/tgbot/TgbotApplication.java
+++ src/main/java/ru/skillfactorydemo/tgbot/TgbotApplication.java
@@ -1,4 +1,4 @@
-package ru.skillfactorydemo;
+package ru.skillfactorydemo.tgbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
