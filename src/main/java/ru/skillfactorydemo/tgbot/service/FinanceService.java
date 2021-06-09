b00b1d39e2a71d5195162d2457020d15bd8540ac
Владимир Охременко <ohremenkov@gmail.com>
2021-06-10 02:14:49 +0300

    Update FinanceService.java
    added function of getting history of spends and incomes

--- src/main/java/ru/skillfactorydemo/tgbot/service/FinanceService.java
+++ src/main/java/ru/skillfactorydemo/tgbot/service/FinanceService.java
@@ -1,6 +1,8 @@
 package ru.skillfactorydemo.tgbot.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.skillfactorydemo.tgbot.entity.Income;
 import ru.skillfactorydemo.tgbot.entity.Spend;
@@ -8,26 +10,93 @@ import ru.skillfactorydemo.tgbot.repository.IncomeRepository;
 import ru.skillfactorydemo.tgbot.repository.SpendRepository;
 
 import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FinanceService {
 
     private static final String ADD_INCOME = "/addincome";
+    private static final String ADD_SPEND = "/addspend";
+    private static final String GET_INCOME_HISTORY = "/getincomehistory";
+    private static final String GET_SPEND_HISTORY = "/getspendhistory";
 
+    @Value("${spring.datasource.url}")
+    private String JDBC;
+    @Value("${spring.datasource.password}")
+    private String PASS;
+    @Value("${spring.datasource.username}")
+    private String USER;
     private final IncomeRepository incomeRepository;
     private final SpendRepository spendRepository;
 
+    public String getOperations(String operationType, Long chatId) {
+        String message = "История отсутствует";
+        if (GET_INCOME_HISTORY.equalsIgnoreCase(operationType))
+        {
+            try
+            {
+                Class.forName("org.postgresql.Driver");
+                String sql = "SELECT income FROM incomes where chat_id = " + chatId;
+                Connection con = DriverManager.getConnection(JDBC,USER,PASS);
+                Statement statement = con.createStatement();
+                ResultSet rs = statement.executeQuery(sql);
+                if (rs.next()){
+                    message = rs.getString("income")+ "\n";
+                    while(rs.next())
+                    {
+                        message = message + rs.getString("income")+ "\n";
+                    }
+                }
+                rs.close();
+                statement.close();
+                con.close();
+
+            }
+            catch (SQLException | ClassNotFoundException e)
+            {
+                log.error("Возникла ошибка запроса", e);
+            }
+        }else
+        {
+            try
+            {
+                Class.forName("org.postgresql.Driver");
+                String sql = "SELECT spend FROM spend where chat_id = " + chatId;
+                Connection con = DriverManager.getConnection(JDBC,USER,PASS);
+                Statement statement = con.createStatement();
+                ResultSet rs = statement.executeQuery(sql);
+                if (rs.next()){
+                    message = rs.getString("spend")+ "\n";
+                    while(rs.next())
+                    {
+                        message = message + rs.getString("spend")+ "\n";
+                    }
+                }
+                rs.close();
+                statement.close();
+                con.close();
+            }
+            catch (SQLException | ClassNotFoundException e)
+            {
+                log.error("Возникла ошибка запроса", e);
+            }
+        }
+        return message;
+    }
+
 
     public String addFinanceOperation(String operationType, String price, Long chatId) {
-        String message;
+        String message = "";
         if (ADD_INCOME.equalsIgnoreCase(operationType)) {
             Income income = new Income();
             income.setChatId(chatId);
             income.setIncome(new BigDecimal(price));
             incomeRepository.save(income);
             message = "Доход в размере " + price + " был успешно добавлен";
-        } else {
+        } else if (ADD_SPEND.equalsIgnoreCase(operationType)){
             Spend spend = new Spend();
             spend.setChatId(chatId);
             spend.setSpend(new BigDecimal(price));
